aeaf21bfeb12c105cf21e08341862fdd1b0cf3f9
VaughnMYA <vaughn@mysoft.co.za>
2025-05-29 15:47:50 +0200

    remove keys

--- refiner/config.py
+++ refiner/config.py
@@ -16,7 +16,7 @@ class Settings(BaseSettings):
     )
 
     REFINEMENT_ENCRYPTION_KEY: str = Field(
-        default="0x043b9d76c6d25f536785c058ce788d8f441c9beff18538a864e2b9b3fcfbb3548c2d3c663907227ce90a3df9837e741d3995bae88b8f912de1a9c906bd02965d53",
+        default="",
         description="Key to symmetrically encrypt the refinement. This is derived from the original file encryption key"
     )
 
@@ -31,7 +31,7 @@ class Settings(BaseSettings):
     # )
 
     PINATA_API_JWT: Optional[str] = Field(
-        default="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySW5mb3JtYXRpb24iOnsiaWQiOiJkOGNjODFiNS04YzljLTQ2NWQtOThkMC1jZTBlZmE3MTliMmQiLCJlbWFpbCI6InZhdWdobkBwb2xpY3lkb2NrLmNvbSIsImVtYWlsX3ZlcmlmaWVkIjp0cnVlLCJwaW5fcG9saWN5Ijp7InJlZ2lvbnMiOlt7ImRlc2lyZWRSZXBsaWNhdGlvbkNvdW50IjoxLCJpZCI6IkZSQTEifSx7ImRlc2lyZWRSZXBsaWNhdGlvbkNvdW50IjoxLCJpZCI6Ik5ZQzEifV0sInZlcnNpb24iOjF9LCJtZmFfZW5hYmxlZCI6ZmFsc2UsInN0YXR1cyI6IkFDVElWRSJ9LCJhdXRoZW50aWNhdGlvblR5cGUiOiJzY29wZWRLZXkiLCJzY29wZWRLZXlLZXkiOiIwMmVhMGI0MmU4OTYyZjBiMzgyZCIsInNjb3BlZEtleVNlY3JldCI6ImRhMDZhNGJmMTE2ODA0ZTgyYWRkZjJmMjYxOGFhMDYzMzFlMDZmMWVjNjcwNzIyZGI3ZDI5OTI5MjRkOTgyZWUiLCJleHAiOjE3Nzk3ODgzNDd9.EqBAIcdJZRUTIY8CRfl_uRCunhHV1nbg8E7AqG8lMQc",
+        default="",
         description="Pinata API JWT"
     )
 
